611e473415a4ca86746d6b7a08552982e5e9c30a
yangjunwei <yuxiao9197@163.com>
2021-08-15 13:56:32 +0800

    优化CustomClassLoader，可以加载指定位置的jar、xar、class、xlass文件

--- src/main/java/com/yuxiao/geek05/week01/CustomClassLoader.java
+++ src/main/java/com/yuxiao/geek05/week01/CustomClassLoader.java
@@ -1,38 +1,81 @@
 package com.yuxiao.geek05.week01;
 
+import sun.misc.IOUtils;
+
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 /**
- * 自定义的类加载器
+ * 自定义的类加载器（支持jar、xar、class、xlass文件的加载）
+ *
  * @author yangjunwei
  * @date 2021-08-06 22:21
  */
-public class CustomClassLoader extends ClassLoader{
+public class CustomClassLoader extends ClassLoader {
 
 
-    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
-        Object hello = new CustomClassLoader().findClass("Hello").newInstance();
-        Method hello1 = hello.getClass().getMethod("hello");
-        hello1.invoke(hello);
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException, IOException {
+        CustomClassLoader classLoader = new CustomClassLoader();
+        classLoader.loadExtXar("lib");
+        Object targetInstance = classLoader.findClass("Hello").newInstance();
+        Method helloMethod = targetInstance.getClass().getMethod("hello");
+        helloMethod.invoke(targetInstance);
     }
 
+
+    /**
+     * 加载xlass打包的xar文件
+     *
+     * @param extLibPath 扩展lib包加载路径
+     */
+    private void loadExtXar(String extLibPath) throws IOException {
+        File extLibFileDir = new File(extLibPath);
+        if (extLibFileDir.isDirectory()) {
+            File[] files = extLibFileDir.listFiles();
+            for (File file : files) {
+                JarFile jarFile = new JarFile(file);
+                Enumeration<JarEntry> entries = jarFile.entries();
+                while (entries.hasMoreElements()) {
+                    JarEntry jarEntry = entries.nextElement();
+                    byte[] bytes = IOUtils.readFully(jarFile.getInputStream(jarEntry), (int) jarEntry.getSize(), true);
+                    // 解码xar中的xlass文件
+                    if (jarFile.getName().endsWith("xar")) {
+                        bytes = getDecodeBytes(bytes);
+                    }
+                    String name = jarEntry.getName();
+                    name = name.substring(0, name.lastIndexOf("."));
+                    Class<?> loadedClass = findLoadedClass(name);
+                    if (loadedClass != null) {
+                        System.out.println(name + "已经加载过，无需重复加载");
+                        return;
+                    }
+                    super.defineClass(name, bytes, 0, bytes.length);
+                }
+            }
+        }
+    }
 
 
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
-        Class<?> aClass = super.findClass(name);
-        if (aClass != null) {
-            return aClass;
+        // 检查是否已加载过，加载过则直接返回
+        Class<?> loadedClass = findLoadedClass(name);
+        if (loadedClass != null) {
+            return loadedClass;
         }
-        String filename = name + ".xlass";
-        // /开头表示从当前工程的根目录查找，""开始标识从当前目录查找
-        // String path = this.getClass().getResource("/").getPath() + filename;
-        try {
-            InputStream resourceAsStream = this.getResourceAsStream(filename);
+        String filename = name.substring(name.lastIndexOf(".") + 1) + ".xlass";
+        try (InputStream resourceAsStream = this.getResourceAsStream(filename)) {
+            // 如果文件为空，则说明不是自定义要加载的类，委托父类进行加载
+            if (resourceAsStream == null) {
+                return super.findClass(name);
+            }
             int length = resourceAsStream.available();
             System.out.println("文件长度:" + length);
             byte[] fileBytes = new byte[length];
@@ -51,6 +94,7 @@ public class CustomClassLoader extends ClassLoader{
 
     /**
      * 处理字节码数据
+     *
      * @param bytes 原字节码数据
      * @return 解码后的字节码
      */
